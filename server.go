307016782cdf579eedb84374086dff44e9fda4fd
padulkemid <bringitdown0@gmail.com>
2020-11-23 15:53:19 +0700

    simple server guide from astaxie ( refresh my mind about go btw )

--- server.go
+++ server.go
@@ -1,8 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func comelAvailable(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	fmt.Println(r.Form)
+	fmt.Printf("path: %s \n", r.URL.Path)
+	fmt.Printf("scheme: %s \n", r.URL.Scheme)
+
+	fmt.Fprintf(w, "What's up?")
+}
 
 func main() {
-	anju := "geminghh"
-	fmt.Printf("Anjuuuu %s \n", anju)
+	// set router
+	http.HandleFunc("/", comelAvailable)
+	err := http.ListenAndServe(":8000", nil)
+
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
