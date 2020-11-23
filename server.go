e44bae5abd25ca3e36c3abf8a0763bcc0134340f
padulkemid <bringitdown0@gmail.com>
2020-11-24 01:41:07 +0700

    Paused

--- server.go
+++ server.go
@@ -6,6 +6,11 @@ import (
 	"net/http"
 )
 
+type Data struct {
+	path   string
+	scheme string
+}
+
 func comelAvailable(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 
@@ -13,12 +18,42 @@ func comelAvailable(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("path: %s \n", r.URL.Path)
 	fmt.Printf("scheme: %s \n", r.URL.Scheme)
 
-	fmt.Fprintf(w, "What's up?")
+	fmt.Fprintf(w, "What's up? %+v", Data{r.URL.Path, r.URL.Scheme})
+}
+
+func loginAja(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("method: %s \n", r.Method)
+
+	if r.Method == "POST" {
+		err := r.ParseForm()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("username: %s", r.Form["username"])
+		fmt.Printf("password: %s", r.Form["password"])
+	}
+}
+
+func checkComel(w http.ResponseWriter, r *http.Request) {
+	status := []string{"available", "not available", "unknown"}
+
+	if r.Method == "POST" {
+		for _, v := range status {
+			if v == r.Form.Get("status") {
+				fmt.Printf("status is: %s", r.Form.Get("status"))
+			}
+		}
+	}
+
 }
 
 func main() {
 	// set router
 	http.HandleFunc("/", comelAvailable)
+	http.HandleFunc("/login", loginAja)
+	http.HandleFunc("/status", checkComel)
 	err := http.ListenAndServe(":8000", nil)
 
 	if err != nil {
